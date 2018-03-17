8ddadb6cc6f5924eed61ea1201a36bccba713f1f
DESKTOP-TQJ9HGT\Neorays <sachin.hn@clusterindia.com>
2018-03-17 12:58:31 +0530

    Updated something

--- DevTest/src/com/cluster/Cluster.java
+++ DevTest/src/com/cluster/Cluster.java
@@ -4,5 +4,7 @@ public class Cluster
 {
 	public static void main(String[] args) {
 		System.out.println("Cluster Info Solutions");
+		System.out.println("Neorays");
+		System.out.println("Ravi Sir");
 	}
 }
